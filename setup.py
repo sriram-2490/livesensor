6242d02bc89a5fe0dbfa41c520e08c390f473062
Sai Venkata Sriram Chowdary Karicheti <saivenkatasriramchowdary.karicheti@slu.edu>
2025-03-20 16:20:19 -0500

    Created Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+def get_requirements()->list[str]:
+    requirements_list =  list[str]=[]
+    return requirements_list
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='prince',
+    author_email='sriramchaowdary@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements() #['pymongo']
+)
